cedb8c2b84f1e12a149dc62cd4422bbd56387991
Michael Adair <mjadair91@gmail.com>
2020-02-16 18:48:42 +0000

    button click effect

--- app.js
+++ app.js
@@ -1,35 +1,44 @@
 function setupCalculator() {
 
+  let numberInput = false
   let userInputOne = []
   let operator = ''
   let userInputTwo = []
 
   const button = document.getElementsByClassName('button')
-  let display = document.querySelector('.user-input')
+  const display = document.querySelector('.user-input')
   display.innerHTML = 0
 
 
 
 
   const addNumber = function () {
-    console.log(display)
-    if (userInputOne.length > 11) {
-      display.style.fontSize('5vw')
+    console.log(userInputOne.length)
+    if (userInputOne.length === 10) {
+      display.style.fontSize = '5vw'
+    } if (userInputOne.length === 14) {
+      display.style.fontSize = '4vw'
     }
+
+    //Reset button===================================================================
     if (this.innerHTML === 'AC') {
       userInputOne = []
+      display.style.fontSize = '7vw'
       return display.innerHTML = 0
     }
+    //Backspace button================================================================
     if (this.innerHTML === '⌫') {
       userInputOne.pop()
       if (userInputOne.length > 0) {
         return display.innerHTML = `${userInputOne.join('')}`
       } else return display.innerHTML = 0
+      //Number input=======================================================================
     } else {
       userInputOne.push(parseFloat(this.innerHTML))
       return display.innerHTML = `${userInputOne.join('')}`
     }
   }
+
 
 
   Array.from(button).forEach((element) => {
