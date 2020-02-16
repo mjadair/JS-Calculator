a91ca0634a0864e68d8e1c4c7c483c9e4781372d
Michael Adair <mjadair91@gmail.com>
2020-02-16 19:57:41 +0000

    added operators

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function setupCalculator() {
 
-  let numberInput = false
-  let userInputOne = []
+  let numberInput = []
+  let equations = []
   let operator = ''
   let userInputTwo = []
 
@@ -14,35 +14,48 @@ function setupCalculator() {
 
   const addNumber = function () {
     //input resizing================================================================
-    console.log(userInputOne.length)
-    if (userInputOne.length === 10) {
+    console.log(this.innerHTML)
+    if (numberInput.length === 10) {
       display.style.fontSize = '5vw'
       display.style.marginTop = '6vw'
-    } if (userInputOne.length === 14) {
+    } if (numberInput.length === 14) {
       display.style.fontSize = '4vw'
       display.style.marginTop = '7vw'
     }
 
     //Reset button===================================================================
     if (this.innerHTML === 'AC') {
-      userInputOne = []
+      numberInput = []
+      equations = []
       display.style.fontSize = '7vw'
       display.style.marginTop = '4vw'
       return display.innerHTML = 0
     }
     //Backspace button================================================================
     if (this.innerHTML === '⌫') {
-      userInputOne.pop()
-      if (userInputOne.length > 0) {
-        return display.innerHTML = `${userInputOne.join('')}`
+      numberInput.pop()
+      if (numberInput.length > 0) {
+        return display.innerHTML = `${numberInput.join('')}`
       } else return display.innerHTML = 0
       //Number input=======================================================================
+    } if (this.innerHTML === '+') {
+      equations.push(parseFloat(...(numberInput.join('').split())))
+      equations.push('+')
+      numberInput = []
+      console.log(equations)
+      return display.innerHTML = `${this.innerHTML}`
+    } if (this.innerHTML === '=') {
+      equations.push(parseFloat(...(numberInput.join('').split())))
+      numberInput = []
+      return display.innerHTML = `${(equations.join(' '))}`
     } else {
-      userInputOne.push(parseFloat(this.innerHTML))
-      return display.innerHTML = `${userInputOne.join('')}`
+      numberInput.push(parseFloat(this.innerHTML))
+      console.log(parseFloat('+'))
+      return display.innerHTML = `${numberInput.join('')}`
     }
   }
 
+  
 
 
   Array.from(button).forEach((element) => {
