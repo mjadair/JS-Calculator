ff149514e51feb2f5c983efb1bfe51be99ac30b7
Michael Adair <mjadair91@gmail.com>
2020-02-16 12:47:01 +0000

    style changes and js selectors

---
+++ app.js
@@ -0,0 +1,17 @@
+function setupCalculator() {
+
+const nine = document.querySelector("#9")
+const eight = document.querySelector("#8")
+const seven = document.querySelector("#7")
+const six = document.querySelector("#6")
+const five = document.querySelector("#5")
+const four = document.querySelector("#4")
+const three = document.querySelector("#3")
+const two = document.querySelector("#2")
+const one = document.querySelector("#1")
+const zero = document.querySelector("#0")
+
+
+
+}
+document.addEventListener('DOMContentLoaded', setupCalculator)
